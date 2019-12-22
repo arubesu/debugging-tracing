9338b9fcc836e16c04ff74a9fdb648c0d2aefd11
Bruno Souza <bruno.arubesu@gmail.com>
2019-12-22 19:52:37 -0300

    Add Project Files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace debugger_tracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
