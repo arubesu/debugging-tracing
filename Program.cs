f49ea5e1ca7158bf7718e1f3a8ff0ddc1b28f295
Bruno Souza <bruno.arubesu@gmail.com>
2019-12-22 22:37:50 -0300

    Add Compiler contitional method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace debugger_tracing
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CounterToN(10);
+		}
+
+
+		/// <summary>
+		/// This Method is only executed in Debug mode
+		/// </summary>
+		/// <param name="n"></param>
+		[Conditional("Debug")]
+		static void CounterToN(int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
 }
